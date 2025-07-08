fusionary/Fusionary.BigCommerceApi

Update Options Return on Update Line

Fusionary.BigCommerce/Operations/Cart/BcApiCart.cs
@@ -11,6 +11,7 @@ public class BcApiCart : IBcApiOperation
         _api = api;
     }
 
+    public BcApiCartGetCheckoutURLs GetCartRedirects() => new(_api);
     public BcApiCartLineAdd AddLineItem() => new(_api);
     public BcApiCartUpdateLine UpdateLineItem() => new(_api);
     public BcApiCartDeleteItem DeleteLineItem() => new(_api);

1-800-Battery/Marketring.BigCommerceApi

Fusionary.BigCommerce/Operations/Cart/BcApiCart.cs
@@ -2,6 +2,7 @@ namespace Fusionary.BigCommerce.Operations;
 
 public class BcApiCart(IBcApi api) : IBcApiOperation
 {
+    public BcApiCartGetCheckoutURLs GetCartRedirects() => new(api);
     public BcApiCartLineAdd AddLineItem() => new(api);
     public BcApiCartUpdateLine UpdateLineItem() => new(api);
     public BcApiCartDeleteItem DeleteLineItem() => new(api);